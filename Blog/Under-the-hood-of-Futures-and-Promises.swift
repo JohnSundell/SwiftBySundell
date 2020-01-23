9ebf839981367f23b3cf4f2d8cafb1c414a30f67
John Sundell <john@sundell.co>
2020-01-23 17:52:09 +0200

    Futures and Promises: Update sample code for remaster

--- Blog/Under-the-hood-of-Futures-and-Promises.swift
+++ Blog/Under-the-hood-of-Futures-and-Promises.swift
@@ -1,93 +1,64 @@
 /**
  *  Swift by Sundell sample code
- *  Copyright (c) John Sundell 2017
+ *  Copyright (c) John Sundell 2020
  *  See LICENSE file for license
  *
- *  Read the blog post at https://swiftbysundell.com/posts/under-the-hood-of-futures-and-promises-in-swift
+ *  Read the article at https://swiftbysundell.com/posts/under-the-hood-of-futures-and-promises-in-swift
  */
 
 import Foundation
-import Unbox
-
-enum Result<Value> {
-    case value(Value)
-    case error(Error)
-}
 
 class Future<Value> {
-    fileprivate var result: Result<Value>? {
+    typealias Result = Swift.Result<Value, Error>
+
+    fileprivate var result: Result? {
+        // Observe whenever a result is assigned, and report it:
         didSet { result.map(report) }
     }
-    private lazy var callbacks = [(Result<Value>) -> Void]()
+    private var callbacks = [(Result) -> Void]()
 
-    func observe(with callback: @escaping (Result<Value>) -> Void) {
+    func observe(using callback: @escaping (Result) -> Void) {
+        // If a result has already been set, call the callback directly:
+        if let result = result {
+            return callback(result)
+        }
+
         callbacks.append(callback)
-        result.map(callback)
     }
 
-    private func report(result: Result<Value>) {
-        for callback in callbacks {
-            callback(result)
-        }
-    }
-}
-
-extension Future {
-    func chained<NextValue>(with closure: @escaping (Value) throws -> Future<NextValue>) -> Future<NextValue> {
-        let promise = Promise<NextValue>()
-
-        observe { result in
-            switch result {
-            case .value(let value):
-                do {
-                    let future = try closure(value)
-
-                    future.observe { result in
-                        switch result {
-                        case .value(let value):
-                            promise.resolve(with: value)
-                        case .error(let error):
-                            promise.reject(with: error)
-                        }
-                    }
-                } catch {
-                    promise.reject(with: error)
-                }
-            case .error(let error):
-                promise.reject(with: error)
-            }
-        }
-
-        return promise
-    }
-
-    func transformed<NextValue>(with closure: @escaping (Value) throws -> NextValue) -> Future<NextValue> {
-        return chained { value in
-            return try Promise(value: closure(value))
-        }
+    private func report(result: Result) {
+        callbacks.forEach { $0(result) }
+        callbacks = []
     }
 }
 
 class Promise<Value>: Future<Value> {
     init(value: Value? = nil) {
         super.init()
-        result = value.map(Result.value)
+
+        // If the value was already known at the time the promise
+        // was constructed, we can report it directly:
+        result = value.map(Result.success)
     }
 
     func resolve(with value: Value) {
-        result = .value(value)
+        result = .success(value)
     }
 
     func reject(with error: Error) {
-        result = .error(error)
+        result = .failure(error)
     }
 }
 
 extension URLSession {
     func request(url: URL) -> Future<Data> {
+        // We'll start by constructing a Promise, that will later be
+        // returned as a Future:
         let promise = Promise<Data>()
 
+        // Perform a data task, just like we normally would:
         let task = dataTask(with: url) { data, _, error in
+            // Reject or resolve the promise, depending on the result:
             if let error = error {
                 promise.reject(with: error)
             } else {
@@ -101,32 +72,62 @@ extension URLSession {
     }
 }
 
-extension Future where Value == Data {
-    func unboxed<NextValue: Unboxable>() -> Future<NextValue> {
-        return transformed { try unbox(data: $0) }
+extension Future {
+    func chained<T>(
+        using closure: @escaping (Value) throws -> Future<T>
+    ) -> Future<T> {
+        // We'll start by constructing a "wrapper" promise that will be
+        // returned from this method:
+        let promise = Promise<T>()
+
+        // Observe the current future:
+        observe { result in
+            switch result {
+            case .success(let value):
+                do {
+                    // Attempt to construct a new future using the value
+                    // returned from the first one:
+                    let future = try closure(value)
+
+                    // Observe the "nested" future, and once it
+                    // completes, resolve/reject the "wrapper" future:
+                    future.observe { result in
+                        switch result {
+                        case .success(let value):
+                            promise.resolve(with: value)
+                        case .failure(let error):
+                            promise.reject(with: error)
+                        }
+                    }
+                } catch {
+                    promise.reject(with: error)
+                }
+            case .failure(let error):
+                promise.reject(with: error)
+            }
+        }
+
+        return promise
     }
 }
 
-extension Future where Value: Savable {
-    func saved(in database: Database) -> Future<Value> {
-        return chained { user in
-            let promise = Promise<Value>()
-
-            database.save(user) {
-                promise.resolve(with: user)
-            }
-
-            return promise
+extension Future {
+    func transformed<T>(
+        with closure: @escaping (Value) throws -> T
+    ) -> Future<T> {
+         chained { value in
+             try Promise(value: closure(value))
         }
     }
 }
 
-class UserLoader {
-    func loadUser(withID id: Int) -> Future<User> {
-        let url = apiConfiguration.urlForLoadingUser(withID: id)
-
-        return urlSession.request(url: url)
-                         .unboxed()
-                         .saved(in: database)
+extension Future where Value == Data {
+    func decoded<T: Decodable>(
+        as type: T.Type = T.self,
+        using decoder: JSONDecoder = .init()
+    ) -> Future<T> {
+        transformed { data in
+            try decoder.decode(T.self, from: data)
+        }
     }
 }
